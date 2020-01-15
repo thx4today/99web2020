0231382698c473555af4807f57c08a43331001aa
thx4today <58929171+thx4today@users.noreply.github.com>
2020-01-15 18:00:40 +0900

    20200115

---
+++ 03script/20200115_script/script.js
@@ -0,0 +1,7 @@
+function over(object){ // 'over'자리에는 어떤 단어든 기입 가능
+			// alert("마우스오버금지"); // 경고창
+			object.src="../../images/banana.png"; //구문의 마지막';'필수
+		}
+		function out(object){
+			object.src="../../images/apple.png"
+		}
